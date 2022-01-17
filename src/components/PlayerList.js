1701086311b93a4c167613f58b08f9dc39ecaffa
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-17 13:01:08 -0500

    finish importing and using helper functions

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,8 +2,15 @@ import React from "react"; //optional
 import matchData from "../data/matchData";
 import playerData from "../data/playerData";
 import Player from "./Player";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
+// importing an object of functions (we can use object destructuring)
+// import { nameOfProperty } from './someFile';
+console.log("preparePlayerData", preparePlayerData);
+console.log("addWinsToPlayers", addWinsToPlayers);
 
 function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
